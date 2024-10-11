38b20a1fb9fc6cde7ee6f9afc3acb53bc4beaeea
ortava <RyanTommyTavares@gmail.com>
2024-10-11 15:10:28 -0700

    Add JavaDoc comment to openTrackInSpotify method

--- SOTDPrototype/app/src/main/java/com/example/sotdprototype/spotify/SpotifyHelper.java
+++ SOTDPrototype/app/src/main/java/com/example/sotdprototype/spotify/SpotifyHelper.java
@@ -7,6 +7,12 @@ import android.content.Intent;
 import android.net.Uri;
 
 public class SpotifyHelper {
+    /**
+        Opens track with the given URI in the user's Spotify app.
+        @param uri          URI identifying the Spotify track to be opened.
+        @param packageName  Name of the application package from which the application is making the request.
+        @param context      Application context from which the application is making the request.
+     */
     public static void openTrackInSpotify(String uri, String packageName, Context context) {
         final String branchLink = "https://spotify.link/content_linking?~campaign=" + packageName + "&$deeplink_path=" + uri + "&$fallback_url=" + uri;
         Intent intent = new Intent(Intent.ACTION_VIEW);
