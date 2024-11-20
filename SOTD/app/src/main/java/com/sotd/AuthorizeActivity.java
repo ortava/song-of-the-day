a54c70a99ea241bf4b7fa3c5d7eb159a39c5897c
ortava <RyanTommyTavares@gmail.com>
2024-11-20 11:51:32 -0800

    Add functionality to clear stored auth data before redirecting to login prompt

--- SOTD/app/src/main/java/com/sotd/AuthorizeActivity.java
+++ SOTD/app/src/main/java/com/sotd/AuthorizeActivity.java
@@ -50,12 +50,12 @@ public class AuthorizeActivity extends AppCompatActivity {
                     SharedPreferences.Editor editor = mSharedPreferences.edit();
                     editor.remove("code_verifier");
                     editor.apply();
-
                     startMainActivity();
                 }
 
                 @Override
                 public void onError(VolleyError error) {
+                    clearSpotifyPreferences();  // Clear any stored authorization data before prompting login.
                     startPrescreenActivity();
                 }
             });
@@ -96,6 +96,12 @@ public class AuthorizeActivity extends AppCompatActivity {
         Intent intent = new Intent(AuthorizeActivity.this, PrescreenActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    private void clearSpotifyPreferences() {
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+        editor.clear();
+        editor.apply();
     }
 
     /// METHODS THAT HELP WITH THE PKCE AUTHORIZATION FLOW (see: https://developer.spotify.com/documentation/web-api/tutorials/code-pkce-flow)
